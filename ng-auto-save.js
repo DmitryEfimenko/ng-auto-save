14053a1817dfbdf5ebf92c354a32305f53841767
Dmitry A. Efimenko <dmitrief@gmail.com>
2015-02-24 12:24:10 -0700

    Update ng-auto-save.js

--- ng-auto-save.js
+++ ng-auto-save.js
@@ -2,80 +2,52 @@
 
 /*
   * Source: https://github.com/DmitryEfimenko/ng-auto-save
+  * 
 */
 
 angular.module('ng-auto-save', [])
-    .directive('autoSave', ['$timeout',
-        function ($timeout) {
-            var fields = [];
+    .service('autoSaveService', [function() {
+        var self = this;
+        self.autoSaveFnName = undefined;
+        self.debounce = undefined;
+
+        self.setAutoSaveFnName = function(name) {
+            self.autoSaveFnName = name;
+        };
+        self.getAutoSaveFnName = function () {
+            return self.autoSaveFnName;
+        };
+        self.setDebounce = function (d) {
+            self.debounce = d;
+        };
+        self.getDebounce = function () {
+            return self.debounce;
+        };
+    }])
+    .directive('autoSave', ['autoSaveService',
+        function (autoSaveService) {
             var savingEls = [];
             var savedEls = [];
 
             return {
                 restrict: 'A',
                 require: 'form',
-                controller: function() {
-                    this.registerField = function(ngModel, field, el) {
-                        fields.push({ ngModel: ngModel, field: field, el: el });
-                    };
+                controller: function($scope, $element, $attrs) {
+                    autoSaveService.setAutoSaveFnName($attrs.autoSave);
+                    autoSaveService.setDebounce($attrs.autoSaveDebounce);
+
                     this.registerSavingEl = function(key, el) {
                         savingEls.push({ key: key, el: el });
                     };
                     this.registerSavedEl = function(key, el) {
                         savedEls.push({ key: key, el: el });
                     };
-                },
-                link: function ($scope, $elem, $attrs, $form) {
-                    var debounce = $attrs.autoSaveDebounce;
-                    var timeout = null;
-
-                    for (var i = 0, l = fields.length; i < l; i++) {
-                        var field = fields[i];
-                        
-                        $scope.$watch(field.ngModel, function(newVal, oldVal) {
-                            if (newVal != oldVal && (!field.lastValidVal || newVal != field.lastValidVal )) {
-                                if ($form.$valid) field.lastValidVal = newVal;
-                                debounceSave(field.field, newVal, field.ngModel);
-                            }
-                        });
-                    }
-                
-                    function debounceSave(prop, value, key) {
-                        if (debounce) {
-                            if (timeout) {
-                                $timeout.cancel(timeout);
-                            }
-                            timeout = $timeout(function() {
-                                save(prop, value, key);
-                            }, debounce);
-                        } else {
-                            save(prop, value, key);
-                        }
-                    }
-
-                    function save(prop, value, key) {
-                        if ($form.$valid) {
-                            changeVisibility(savingEls, key, true);
-                            changeVisibility(savedEls, key, false);
-                            changeEnabled(fields, key, false);
-
-                            $scope[$attrs.autoSave](prop, value).then(
-                                function() {
-                                    changeVisibility(savingEls, key, false);
-                                    changeVisibility(savedEls, key, true);
-                                    changeEnabled(fields, key, true);
-                                },
-                                function(error) {
-                                    console.log(error);
-                                    changeVisibility(savingEls, key, false);
-                                    changeEnabled(fields, key, true);
-                                }
-                            );
-                        } else {
-                            changeVisibility(savedEls, key, false);
-                            changeEnabled(fields, key, true);
-                        }
-                    }
+                    this.changeSavingVisibility = function(key, shouldShow) {
+                        changeVisibility(savingEls, key, shouldShow);
+                    };
+                    this.changeSavedVisibility = function(key, shouldShow) {
+                        changeVisibility(savedEls, key, shouldShow);
+                    };
 
                     function changeVisibility(elArr, key, shouldShow) {
                         for (var j = 0, jl = elArr.length; j < jl; j++) {
@@ -88,31 +60,76 @@ angular.module('ng-auto-save', [])
                             }
                         }
                     }
-
-                    function changeEnabled(elArr, key, shouldEnable) {
-                        for (var j = 0, jl = elArr.length; j < jl; j++) {
-                            if (elArr[j].ngModel == key) {
-                                if (shouldEnable) {
-                                    elArr[j].el.removeAttr('disabled');
-                                    elArr[j].el[0].focus();
-                                } else
-                                    elArr[j].el.attr('disabled', 'disabled');
-                                break;
-                            }
-                        }
-                    }
                 }
             };
         }
     ])
-    .directive('autoSaveField', [
-        function () {
+    .directive('autoSaveField', ['$timeout', 'autoSaveService',
+        function ($timeout, autoSaveService) {
             return {
                 restrict: 'A',
-                require: ['^autoSave', 'ngModel'],
+                require: ['^autoSave', '^form', 'ngModel'],
                 link: function ($scope, $elem, $attrs, $ctrls) {
                     var autoSaveCtrl = $ctrls[0];
-                    autoSaveCtrl.registerField($attrs.ngModel, $attrs.autoSaveField, $elem);
+                    var form = $ctrls[1];
+                    var lastValidVal = undefined;
+                    var ngModel = $attrs.ngModel;
+                    var field = $attrs.autoSaveField;
+                    var timeout = null;
+                    var autoSaveFnName = autoSaveService.getAutoSaveFnName();
+                    var debounce = autoSaveService.getDebounce();
+
+                    $scope.$watch(ngModel, function(newVal, oldVal) {
+                        if (newVal != oldVal && (lastValidVal || newVal != lastValidVal )) {
+                            if (form.$valid) lastValidVal = newVal;
+                            debounceSave(field, newVal, ngModel);
+                        }
+                    });
+
+                    function debounceSave(prop, value, key) {
+                        if (debounce) {
+                            if (timeout) {
+                                $timeout.cancel(timeout);
+                            }
+                            timeout = $timeout(function () {
+                                save(prop, value, key);
+                            }, debounce);
+                        } else {
+                            save(prop, value, key);
+                        }
+                    }
+
+                    function save(prop, value, key) {
+                        if (form.$valid) {
+                            autoSaveCtrl.changeSavingVisibility(key, true);
+                            autoSaveCtrl.changeSavedVisibility(key, false);
+                            changeEnabled(false);
+                            
+                            $scope[autoSaveFnName](prop, value).then(
+                                function () {
+                                    autoSaveCtrl.changeSavingVisibility(key, false);
+                                    autoSaveCtrl.changeSavedVisibility(key, true);
+                                    changeEnabled(true);
+                                },
+                                    function (error) {
+                                    console.log(error);
+                                    autoSaveCtrl.changeSavingVisibility(key, false);
+                                    changeEnabled(true);
+                                }
+                            );
+                        } else {
+                            autoSaveCtrl.changeSavedVisibility(key, false);
+                            changeEnabled(true);
+                        }
+                    }
+
+                    function changeEnabled(shouldEnable) {
+                        if (shouldEnable) {
+                            $elem.removeAttr('disabled');
+                            $elem[0].focus();
+                        } else
+                            $elem.attr('disabled', 'disabled');
+                    }
                 }
             };
         }
