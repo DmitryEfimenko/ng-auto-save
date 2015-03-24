89ad34791ccb7e499d831fb42dea0d331f379713
Dmitry A. Efimenko <dmitrief@gmail.com>
2015-03-24 10:14:31 -0700

    Now works correctlyin ng-repeat

--- ng-auto-save.js
+++ ng-auto-save.js
@@ -6,184 +6,180 @@
 */
 
 angular.module('ng-auto-save', [])
-    .service('autoSaveService', [
-        function() {
-            var self = this;
-            self.autoSaveFnName = undefined;
-            self.key = undefined;
-            self.debounce = undefined;
-
-            self.setAutoSaveFnName = function(name) {
-                self.autoSaveFnName = name;
-            };
-            self.getAutoSaveFnName = function() {
-                return self.autoSaveFnName;
-            };
-            self.setKey = function(k) {
-                self.key = k;
-            };
-            self.getKey = function() {
-                return self.key;
-            };
-            self.setDebounce = function(d) {
-                self.debounce = d;
-            };
-            self.getDebounce = function() {
-                return self.debounce;
-            };
-        }
-    ])
     .directive('autoSave', [
-        'autoSaveService',
-        function(autoSaveService) {
-            var savingEls = [];
-            var savedEls = [];
-
-            return {
-                restrict: 'A',
-                require: 'form',
-                controller: function($scope, $element, $attrs) {
-                    autoSaveService.setAutoSaveFnName($attrs.autoSave);
-                    autoSaveService.setKey($scope.$eval($attrs.autoSaveKey));
-                    autoSaveService.setDebounce($attrs.autoSaveDebounce);
-
-                    this.registerSavingEl = function(col, el) {
-                        savingEls.push({ col: col, el: el });
-                    };
-                    this.registerSavedEl = function(col, el) {
-                        savedEls.push({ col: col, el: el });
-                    };
-                    this.changeSavingVisibility = function(col, shouldShow) {
-                        changeVisibility(savingEls, col, shouldShow);
-                    };
-                    this.changeSavedVisibility = function(col, shouldShow) {
-                        changeVisibility(savedEls, col, shouldShow);
-                    };
-
-                    function changeVisibility(elArr, col, shouldShow) {
-                        for (var j = 0, jl = elArr.length; j < jl; j++) {
-                            if (elArr[j].col == col) {
-                                if (shouldShow)
-                                    elArr[j].el.removeClass('ng-hide');
-                                else
-                                    elArr[j].el.addClass('ng-hide');
-                                break;
-                            }
+    function () {
+        var savingEls = [];
+        var savedEls = [];
+        
+        return {
+            restrict: 'A',
+            require: 'form',
+            controller: function ($scope, $element, $attrs) {
+                var self = this;
+                self.key = $scope.$eval($attrs.autoSaveKey);
+                self.autoSaveFnName = $attrs.autoSave;
+                self.debounce = $attrs.autoSaveDebounce;
+                
+                self.registerSavingEl = function (col, el) {
+                    savingEls.push({ col: col, key: self.key, el: el });
+                };
+                self.registerSavedEl = function (col, el) {
+                    savedEls.push({ col: col, key: self.key, el: el });
+                };
+                self.changeSavingVisibility = function (col, shouldShow) {
+                    changeVisibility(savingEls, col, shouldShow);
+                };
+                self.changeSavedVisibility = function (col, shouldShow) {
+                    changeVisibility(savedEls, col, shouldShow);
+                };
+                
+                function changeVisibility(elArr, col, shouldShow) {
+                    for (var j = 0, jl = elArr.length; j < jl; j++) {
+                        if (elArr[j].col == col && elArr[j].key == self.key) {
+                            if (shouldShow)
+                                elArr[j].el.removeClass('ng-hide');
+                            else
+                                elArr[j].el.addClass('ng-hide');
+                            break;
                         }
                     }
+                }
 
-                },
-                link: function($scope) {
-                    $scope.$on('$destroy', function() {
-                        savingEls.length = 0;
-                        savedEls.length = 0;
+            },
+            link: function ($scope) {
+                $scope.$on('$destroy', function () {
+                    savingEls.length = 0;
+                    savedEls.length = 0;
+                });
+            }
+        };
+    }
+])
+    .directive('autoSaveField', [
+    '$timeout',
+    function ($timeout) {
+        return {
+            restrict: 'A',
+            require: ['^autoSave', '^form', 'ngModel'],
+            link: function ($scope, $elem, $attrs, $ctrls) {
+                var autoSaveCtrl = $ctrls[0];
+                var form = $ctrls[1];
+                var lastValidVal = undefined;
+                var ngModel = $attrs.ngModel;
+                var field = $attrs.autoSaveField;
+                var timeout = null;
+                var autoSaveFnName = autoSaveCtrl.autoSaveFnName;
+                var debounce = autoSaveCtrl.debounce;
+                var key = autoSaveCtrl.key;
+                var queue = [];
+                
+                if (!$scope.autoSaving) $scope.autoSaving = {};
+                if (!$scope.autoSaving[field]) $scope.autoSaving[field] = {};
+                if (!$scope.autoSaving[field][key]) $scope.autoSaving[field][key] = false;
+                
+                if (!$scope.autoSaved) $scope.autoSaved = {};
+                if (!$scope.autoSaved[field]) $scope.autoSaved[field] = {};
+                if (!$scope.autoSaved[field][key]) $scope.autoSaved[field][key] = false;
+                
+                if (key) {
+                    $scope.$watch(ngModel, function (newVal, oldVal) {
+                        if (newVal != oldVal && (lastValidVal || newVal != lastValidVal)) {
+                            if (form.$valid) lastValidVal = newVal;
+                            debounceSave(field, newVal);
+                        }
                     });
                 }
-            };
-        }
-    ])
-    .directive('autoSaveField', [
-        '$timeout', 'autoSaveService',
-        function($timeout, autoSaveService) {
-            return {
-                restrict: 'A',
-                require: ['^autoSave', '^form', 'ngModel'],
-                link: function($scope, $elem, $attrs, $ctrls) {
-                    var autoSaveCtrl = $ctrls[0];
-                    var form = $ctrls[1];
-                    var lastValidVal = undefined;
-                    var ngModel = $attrs.ngModel;
-                    var field = $attrs.autoSaveField;
-                    var timeout = null;
-                    var autoSaveFnName = autoSaveService.getAutoSaveFnName();
-                    var debounce = autoSaveService.getDebounce();
-                    var key = autoSaveService.getKey();
-                    var queue = [];
-                    var saving = false;
-
-                    if (key) {
-                        $scope.$watch(ngModel, function(newVal, oldVal) {
-                            if (newVal != oldVal && (lastValidVal || newVal != lastValidVal)) {
-                                if (form.$valid) lastValidVal = newVal;
-                                debounceSave(field, newVal);
-                            }
-                        });
-                    }
-
-                    function debounceSave(col, value) {
-                        if (debounce) {
-                            if (timeout) {
-                                $timeout.cancel(timeout);
-                            }
-                            timeout = $timeout(function() {
-                                queue.push({ col: col, value: value });
-                                runQueue();
-                            }, debounce);
-                        } else {
+                
+                function debounceSave(col, value) {
+                    if (debounce) {
+                        if (timeout) {
+                            $timeout.cancel(timeout);
+                        }
+                        timeout = $timeout(function () {
                             queue.push({ col: col, value: value });
                             runQueue();
-                        }
+                        }, debounce);
+                    } else {
+                        queue.push({ col: col, value: value });
+                        runQueue();
                     }
-
-                    function runQueue() {
-                        if (!saving) {
-                            var args = queue.shift();
-                            if (args)
-                                save(args.col, args.value, runQueue);
-                        }
-                    }
-
-                    function save(col, value, cb) {
-                        if (form.$valid) {
-                            saving = true;
-                            autoSaveCtrl.changeSavingVisibility(col, true);
-                            autoSaveCtrl.changeSavedVisibility(col, false);
-
-                            $scope[autoSaveFnName](col, value, key).then(
-                                function() {
-                                    autoSaveCtrl.changeSavingVisibility(col, false);
-                                    autoSaveCtrl.changeSavedVisibility(col, true);
-                                    saving = false;
-                                    cb();
-                                },
-                                function(error) {
-                                    console.log(error);
-                                    autoSaveCtrl.changeSavingVisibility(col, false);
-                                    saving = false;
-                                    cb();
-                                }
-                            );
-                        } else {
-                            autoSaveCtrl.changeSavedVisibility(col, false);
-                            cb();
-                        }
-                    }
-
                 }
-            };
-        }
-    ])
+                
+                function runQueue() {
+                    if (!getSaving()) {
+                        var args = queue.shift();
+                        if (args)
+                            save(args.col, args.value, runQueue);
+                    }
+                }
+                
+                function getSaving() {
+                    return $scope.autoSaving[field][key];
+                }
+                
+                function setSaving(val) {
+                    $scope.autoSaving[field][key] = val;
+                }
+                
+                function setSaved(val) {
+                    $scope.autoSaved[field][key] = val;
+                }
+                
+                function save(col, value, cb) {
+                    if (form.$valid) {
+                        setSaving(true);
+                        autoSaveCtrl.changeSavingVisibility(col, true);
+                        autoSaveCtrl.changeSavedVisibility(col, false);
+                        
+                        $scope[autoSaveFnName](col, value, key).then(
+                            function () {
+                                autoSaveCtrl.changeSavingVisibility(col, false);
+                                autoSaveCtrl.changeSavedVisibility(col, true);
+                                setSaving(false);
+                                setSaved(true);
+                                $timeout(function () {
+                                    setSaved(false);
+                                }, 3000);
+                                cb();
+                            },
+                                function (error) {
+                                console.log(error);
+                                autoSaveCtrl.changeSavingVisibility(col, false);
+                                setSaving(false);
+                                cb();
+                            }
+                        );
+                    } else {
+                        autoSaveCtrl.changeSavedVisibility(col, false);
+                        cb();
+                    }
+                }
+
+            }
+        };
+    }
+])
     .directive('autoSaving', [
-        function() {
-            return {
-                restrict: 'A',
-                require: '^autoSave',
-                link: function($scope, $elem, $attrs, autoSaveCtrl) {
-                    $elem.addClass('ng-hide');
-                    autoSaveCtrl.registerSavingEl($attrs.autoSaving, $elem);
-                }
-            };
-        }
-    ])
+    function () {
+        return {
+            restrict: 'A',
+            require: '^autoSave',
+            link: function ($scope, $elem, $attrs, autoSaveCtrl) {
+                $elem.addClass('ng-hide');
+                autoSaveCtrl.registerSavingEl($attrs.autoSaving, $elem);
+            }
+        };
+    }
+])
     .directive('autoSaved', [
-        function() {
-            return {
-                restrict: 'A',
-                require: '^autoSave',
-                link: function($scope, $elem, $attrs, autoSaveCtrl) {
-                    $elem.addClass('ng-hide');
-                    autoSaveCtrl.registerSavedEl($attrs.autoSaved, $elem);
-                }
-            };
-        }
-    ]);
+    function () {
+        return {
+            restrict: 'A',
+            require: '^autoSave',
+            link: function ($scope, $elem, $attrs, autoSaveCtrl) {
+                $elem.addClass('ng-hide');
+                autoSaveCtrl.registerSavedEl($attrs.autoSaved, $elem);
+            }
+        };
+    }
+]);
